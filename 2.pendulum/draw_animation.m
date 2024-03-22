806f10605382191da6b698cd440db60dfad45742
TAEM1N2 <taemin1212@gmail.com>
2024-03-22 18:54:21 +0900

    3. cartpole 추가

---
+++ 2.pendulum/draw_animation.m
@@ -0,0 +1,19 @@
+% for Mass-Spring Simulation
+%
+ 
+function draw_animation(x1,z1)
+global p p1
+
+% Addpoints
+addpoints(p,[0 x1],[0 z1]);
+addpoints(p1,[x1-0.02 x1+0.02 x1+0.02 x1-0.02 x1-0.02],...
+             [z1-0.05 z1-0.05 z1+0.05 z1+0.05 z1-0.05]);
+
+%hold on
+drawnow
+%hold on
+pause(0.01);
+end
+
+
+
--- 3.cartpole/draw_animation.m
+++ 3.cartpole/draw_animation.m
@@ -1,14 +1,14 @@
 % for Mass-Spring Simulation
 %
  
-function draw_animation(x1,z1)
-global p p1
+function draw_animation(x1, y1, x2, y2)
+global p p1 p2
 
 % Addpoints
-addpoints(p,[0 x1],[0 z1]);
-addpoints(p1,[x1-0.02 x1+0.02 x1+0.02 x1-0.02 x1-0.02],...
-             [z1-0.05 z1-0.05 z1+0.05 z1+0.05 z1-0.05]);
-addpoints(pw, [])
+
+addpoints(p,[x1-0.04 x1+0.04 x1+0.04 x1-0.04 x1-0.04], [y1-0.05 y1-0.05 y1+0.05 y1+0.05 y1-0.05]);
+addpoints(p1,[x1 x2],[y1 y2]);
+addpoints(p2,[x2-0.02 x2+0.02 x2+0.02 x2-0.02 x2-0.02], [y2-0.02 y2-0.02 y2+0.02 y2+0.02 y2-0.02]);
 
 %hold on
 drawnow
